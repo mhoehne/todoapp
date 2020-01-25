dd1a4fb568855312ea0aefc54d3ec8ebf54e4dd3
mhoehne <martin-hoehne@live.de>
2020-01-25 16:52:57 +0100

    first commit

---
+++ todo_repository.js
@@ -0,0 +1,51 @@
+const todos = [
+  {
+    id:1,
+    Titel:'Aufgaben',
+    Datum:'2020/01/25',
+    Author:'Martin',
+    Kategorie:'Uni',
+    Fertig: false,
+  }]
+let idcounter = 1
+
+function insert(Aufgabe) {
+  Aufgabe.id= ++idcounter
+  todos.push(Aufgabe)
+  return Aufgabe
+}
+
+function getall() {
+  return todos
+}
+
+function getbyid(id) {
+  for(i=0; i<todos.length; i++) {
+    if (id == todos[i].id) {
+      return todos[i]
+    }
+  }
+}
+
+function updatebyid (id, body) {
+  const Aufgabe = getbyid(id)
+    if (Aufgabe) {
+      Aufgabe.Titel = body.Titel || Aufgabe.Titel
+      Aufgabe.Datum = body.Datum || Aufgabe.Datum
+      Aufgabe.Author= body.Author || Aufgabe.Author
+      Aufgabe.Kategorie = body.Kategorie || Aufgabe.Kategorie
+      Aufgabe.Fertig = body.Fertig || Aufgabe.Fertig
+    }
+}
+
+function deletebyid(id) {
+  const Aufgabe = getbyid(id)
+    if (Aufgabe) {
+      const index = todos.indexOf(Aufgabe);
+      todos.splice(index, 1); // removes 1 element from position i
+    }
+}
+
+module.exports = {
+  insert, getall, getbyid, updatebyid, deletebyid
+}
---
+++ todoapp.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = 3000
+const todo_repository = require('./todo_repository')
+
+app.use(cors());
+app.use(express.json()) //for parsing application/JSON
+
+//POST
+app.post('/todo', function(req, res, next) {
+  res.set('Content-Type', 'application/json')
+  const Aufgabe = todo_repository.insert(req.body)
+  return res.status(201).send(JSON.stringify(Aufgabe))
+});
+
+//GET ALL
+app.get('/todos', function(req, res, next) {
+  res.set('Content-Type', 'application/json')
+  return res.send(JSON.stringify(todo_repository.getall()))
+});
+
+//GET by ID
+app.get('/todo/:id', function(req, res, next) {
+  res.set('Content-Type', 'application/json')
+  const Aufgabe = todo_repository.getbyid (req.params.id)
+  if (Aufgabe) {
+    return res.status(200).send(JSON.stringify(Aufgabe))
+  }
+
+  return res.sendStatus(404)
+});
+
+//PUT
+app.put('/todo/:id', function(req, res, next) {
+  res.set('Content-Type', 'application/json')
+  const Aufgabe = todo_repository.getbyid(req.params.id)
+    if (Aufgabe) {
+      todo_repository.updatebyid(req.params.id, req.body)
+      return res.status(200).send(JSON.stringify(Aufgabe))
+    }
+
+  return res.sendStatus(404)
+});
+
+//DEL
+app.delete('/todo/:id', function(req, res, next) {
+  res.set('Content-Type', 'application/json')
+  Aufgabe = todo_repository.getbyid(req.params.id)
+    if (Aufgabe) {
+      todo_repository.deletebyid(req.params.id)
+      return res.sendStatus(200)
+    }
+  return res.sendStatus(404)
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
